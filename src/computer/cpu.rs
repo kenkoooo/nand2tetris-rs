945830ffc2deab980c90c51d72efae2ff64bf9e3
Kenko Nakamura <kenkou.n@gmail.com>
2018-11-05 13:10:44 +0900

    WIP

--- src/computer/cpu.rs
+++ src/computer/cpu.rs
@@ -10,12 +10,154 @@ pub struct CPU {
 }
 
 impl CPU {
-    pub fn calc(&mut self, input: [bool; 16], instruction: [bool; 16], reset: bool) {
-        use self::gates::{and, not};
-        let a_instruction_p = not(instruction[0]);
-        let write_m = and(instruction[12], not(a_instruction_p));
+    pub fn new() -> Self {
+        CPU {
+            data_register: Register::new(),
+            address_register: Register::new(),
+            pc: ProgramCounter::new(),
+            alu_out: [false; 16],
+        }
+    }
+    pub fn calc(
+        &mut self,
+        input: [bool; 16],
+        instruction: [bool; 16],
+        reset: bool,
+    ) -> ([bool; 16], bool, [bool; 15], [bool; 15]) {
+        use self::gates::{and, not, or};
+        let a_instruction_p = not(instruction[15]);
+        let write_m = and(instruction[15], instruction[3]);
 
         let new_a = gates::mux16(self.alu_out, instruction, a_instruction_p);
-        let store_ap1
+        let store_ap1 = and(instruction[5], instruction[15]);
+        let store_ap = or(store_ap1, a_instruction_p);
+        self.address_register.tick(new_a, store_ap);
+        let store_a = self.address_register.tock();
+        let address_m = [
+            store_a[0],
+            store_a[1],
+            store_a[2],
+            store_a[3],
+            store_a[4],
+            store_a[5],
+            store_a[6],
+            store_a[7],
+            store_a[8],
+            store_a[9],
+            store_a[10],
+            store_a[11],
+            store_a[12],
+            store_a[13],
+            store_a[14],
+        ];
+
+        let alu_in_am = gates::mux16(store_a, input, instruction[12]);
+        let (out_m, zr, ng) = alu::alu(
+            self.data_register.tock(),
+            alu_in_am,
+            instruction[11],
+            instruction[10],
+            instruction[9],
+            instruction[8],
+            instruction[7],
+            instruction[6],
+        );
+        self.alu_out = out_m;
+        let store_dp = and(instruction[4], instruction[15]);
+
+        self.data_register.tick(self.alu_out, store_dp);
+
+        let zr_inv = not(zr);
+        let ng_inv = not(ng);
+
+        let jgt1 = and(zr_inv, ng_inv);
+        let jgt = and(instruction[0], jgt1);
+
+        let jeq = and(zr, instruction[1]);
+        let load1 = or(jeq, jgt);
+
+        let jlt = and(ng, instruction[2]);
+        let load2 = or(jlt, load1);
+        let load3 = and(load2, instruction[15]);
+
+        self.pc.tick(store_a, reset, load3, true);
+        let out = self.pc.tock();
+        let pc = [
+            out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7], out[8], out[9],
+            out[10], out[11], out[12], out[13], out[14],
+        ];
+        (out_m, write_m, address_m, pc)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tools;
+
+    fn convert16(x: i16) -> [bool; 16] {
+        let mut result = [false; 16];
+        for i in 0..15 {
+            result[i] = (1 << i) & x != 0;
+        }
+        result[15] = x < 0;
+        result
+    }
+
+    fn convert16_str(x: &str) -> [bool; 16] {
+        let mut res = [false; 16];
+        for i in 0..16 {
+            res[i] = &x[(15 - i)..(15 - i + 1)] == "1";
+        }
+        res
+    }
+
+    fn bit_16_to_15(x: [bool; 16]) -> [bool; 15] {
+        let mut res = [false; 15];
+        for i in 0..15 {
+            res[i] = x[i];
+        }
+        res
+    }
+
+    #[test]
+    fn cpu_test() {
+        let t = tools::read_test_data("tests/05/CPU.cmp").unwrap();
+        let mut iter = t[1..].iter().map(|t| {
+            let is_set = t[1].chars().next_back().unwrap() == '+';
+            let input = convert16(t[2].parse::<i16>().unwrap());
+            println!("{}", t[3]);
+            let instruction = convert16_str(&t[3]);
+            let reset = t[4] == "1";
+            let out = t[5].parse::<i16>().ok().map(|t| convert16(t));
+            let write_m = t[6] == "1";
+            let address = convert16(t[7].parse::<i16>().unwrap());
+            let pc = convert16(t[8].parse::<i16>().unwrap());
+
+            (
+                is_set,
+                input,
+                instruction,
+                reset,
+                out,
+                write_m,
+                bit_16_to_15(address),
+                bit_16_to_15(pc),
+            )
+        });
+
+        let mut cpu = CPU::new();
+
+        while let Some((is_set, input, instruction, reset, _, _, _, _)) = iter.next() {
+            assert!(is_set);
+            let (out, write_m, address, pc) = cpu.calc(input, instruction, reset);
+            let (_, _, _, _, t_out, t_write_m, t_address, t_pc) = iter.next().unwrap();
+            if let Some(t_out) = t_out {
+                assert_eq!(out, t_out);
+            }
+            assert_eq!(write_m, t_write_m);
+            assert_eq!(address, t_address);
+            assert_eq!(pc, t_pc);
+        }
     }
 }
--- src/tools.rs
+++ src/tools.rs
@@ -1,24 +1,24 @@
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 
 pub fn parse_test_cases(s: &str) -> Vec<Vec<&str>> {
-    s.trim()
-        .split('\n')
-        .map(|line| line.split('|').map(|v| v.trim()).collect())
-        .collect()
+        s.trim().split('\n')
+                .map(|line| line.split('|').map(|v| v.trim()).collect())
+                .collect()
 }
 
-pub fn read_file(filename: &str) -> std::io::Result<String> {
-    let mut file = File::open(filename)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(contents)
+pub fn read_file(filename: &str) -> io::Result<String> {
+        let mut file = File::open(filename)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        Ok(contents)
 }
 
-pub fn read_test_data(filename: &str) -> std::io::Result<Vec<Vec<String>>> {
-    let text = read_file(filename)?;
-    Ok(parse_test_cases(&text)
-        .iter()
-        .map(|t| t.iter().map(|&t| t.to_owned()).collect())
-        .collect())
+pub fn read_test_data(filename: &str) -> io::Result<Vec<Vec<String>>> {
+        let text = read_file(filename)?;
+        Ok(parse_test_cases(&text)
+                .iter()
+                .map(|t| t.iter().map(|&t| t.to_owned()).collect())
+                .collect())
 }
