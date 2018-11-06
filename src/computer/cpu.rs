5ea2a98902631fa8d4f0d64420a12b9b81e82800
kenkoooo <kenkou.n@gmail.com>
2018-11-06 12:23:22 +0900

    CPU is working nowcargo testcargo testcargo test

--- src/computer/cpu.rs
+++ src/computer/cpu.rs
@@ -7,8 +7,27 @@ pub struct CPU {
     address_register: Register,
     pc: ProgramCounter,
 
-    alu_out: ([bool; 16], bool, bool),
     a_out: [bool; 16],
+    d_out: [bool; 16],
+}
+
+fn mux_alu(
+    a_out: [bool; 16],
+    input: [bool; 16],
+    instruction: [bool; 16],
+    d_out: [bool; 16],
+) -> ([bool; 16], bool, bool) {
+    let a_m_out = gates::mux16(a_out, input, instruction[12]);
+    alu::alu(
+        d_out,
+        a_m_out,
+        instruction[11],
+        instruction[10],
+        instruction[9],
+        instruction[8],
+        instruction[7],
+        instruction[6],
+    )
 }
 
 impl CPU {
@@ -18,25 +37,26 @@ impl CPU {
             address_register: Register::new(),
             pc: ProgramCounter::new(),
 
-            alu_out: ([false; 16], false, false),
             a_out: [false; 16],
+            d_out: [false; 16],
         }
     }
 
     pub fn tick(&mut self, input: [bool; 16], instruction: [bool; 16], reset: bool) {
         use self::gates::{and, not, or};
 
+        let (alu_out, zr_out, ng_out) = mux_alu(self.a_out, input, instruction, self.d_out);
+
         let a_instruction = not(instruction[15]);
         let c_instruction = not(a_instruction);
         let alu_to_a = and(c_instruction, instruction[5]);
-        let a_reg_in = gates::mux16(instruction, self.alu_out.0, alu_to_a);
+        let a_reg_in = gates::mux16(instruction, alu_out, alu_to_a);
         let load_a = or(a_instruction, alu_to_a);
         self.address_register.tick(a_reg_in, load_a);
 
         let load_d = and(c_instruction, instruction[4]);
-        self.data_register.tick(self.alu_out.0, load_d);
+        self.data_register.tick(alu_out, load_d);
 
-        let (_, zr_out, ng_out) = self.alu_out;
         let jeq = and(zr_out, instruction[1]);
         let jlt = and(ng_out, instruction[2]);
         let zero_or_neg = or(zr_out, ng_out);
@@ -58,17 +78,7 @@ impl CPU {
 
         let d_out = self.data_register.tock();
         let a_out = self.address_register.tock();
-        let a_m_out = gates::mux16(a_out, input, instruction[12]);
-        let (out_m, zr_out, ng_out) = alu::alu(
-            d_out,
-            a_m_out,
-            instruction[11],
-            instruction[10],
-            instruction[9],
-            instruction[8],
-            instruction[7],
-            instruction[6],
-        );
+        let (out_m, _, _) = mux_alu(a_out, input, instruction, d_out);
         let pc = self.pc.tock();
         let pc = [
             pc[0], pc[1], pc[2], pc[3], pc[4], pc[5], pc[6], pc[7], pc[8], pc[9], pc[10], pc[11],
@@ -83,7 +93,7 @@ impl CPU {
         let write_m = and(c_instruction, instruction[3]);
 
         self.a_out = a_out;
-        self.alu_out = (out_m, zr_out, ng_out);
+        self.d_out = d_out;
 
         (out_m, write_m, address_m, pc)
     }
