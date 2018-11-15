9583af9f9660ac0fe0a5ca12e9f28727e15a03bb
kenkoooo <kenkou.n@gmail.com>
2018-11-15 14:07:37 +0900

    Refactoring

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,160 +1,172 @@
 pub fn compile(x: &Vec<&str>, file_label: &str) -> Result<Vec<String>, String> {
-    let mut output = vec![];
-    output.add("@256");
-    output.add("D=A");
-    output.add("@SP");
-    output.add("M=D");
+    let mut writer = VirtualMachineWriter { output: vec![] };
+    writer.add_str("@256");
+    writer.add_str("D=A");
+    writer.add_str("@SP");
+    writer.add_str("M=D");
 
     for &line in x.iter() {
         let line = line.split("//").next().unwrap().trim();
         if line.len() == 0 {
             continue;
         }
+
         let split_line: Vec<&str> = line.trim().split(" ").collect();
         assert!(split_line.len() > 0);
         match split_line[0] {
             "push" => {
-                if split_line.len() >= 3 && split_line[1] == "constant" {
-                    match split_line[2].parse::<i16>() {
-                        Ok(n) => {
-                            output.add(&format!("@{}", n));
-                            output.add("D=A");
-                            output.add("@SP");
-                            output.add("A=M");
-                            output.add("M=D");
-                            output.add("A=A+1");
-                            output.add("D=A");
-                            output.add("@SP");
-                            output.add("M=D");
-                        }
-                        Err(_) => return Err(format!("Parse Error: {}", line)),
-                    }
-                } else {
-                    unimplemented!();
+                if split_line.len() < 3 || writer.push(split_line[1], split_line[2]).is_err() {
+                    return Err(format!("Parse error: {}", line));
                 }
             }
             "eq" | "gt" | "lt" => {
-                output.get_2_numbers_from_stack();
+                writer.get_2_numbers_from_stack();
                 match split_line[0] {
                     "eq" => {
-                        let equal_label = format!("{}{}", "equal_label", output.len());
-                        let finish_label = format!("{}{}", "finish_label", output.len());
+                        let equal_label = writer.generate_label("equal_label");
+                        let finish_label = writer.generate_label("finish_label");
 
                         // if D!=M, jump to equal_label and D will be 0
-                        output.add("D=M-D");
-                        output.add(&format!("@{}", equal_label));
-                        output.add("D;JNE"); // if D!=0, jump to equal_label
+                        writer.add_str("D=M-D");
+                        writer.add_str(&format!("@{}", equal_label));
+                        writer.add_str("D;JNE"); // if D!=0, jump to equal_label
 
                         // if D==M, D will be -1 and jump to finish_label
-                        output.add("D=-1");
-                        output.add(&format!("@{}", finish_label));
-                        output.add("0;JMP"); // jump to finish_label
+                        writer.add_str("D=-1");
+                        writer.add_str(&format!("@{}", finish_label));
+                        writer.add_str("0;JMP"); // jump to finish_label
 
-                        output.add(&format!("({})", equal_label));
-                        output.add("D=0");
+                        writer.add_str(&format!("({})", equal_label));
+                        writer.add_str("D=0");
 
-                        output.add(&format!("({})", finish_label));
+                        writer.add_str(&format!("({})", finish_label));
                     }
                     "lt" => {
                         // true if D > M
-                        let larger_label = format!("{}{}", "larger_label", output.len());
-                        let finish_label = format!("{}{}", "finish_label", output.len());
+                        let larger_label = writer.generate_label("larger_label");
+                        let finish_label = writer.generate_label("finish_label");
 
                         // if , jump to larger_label and D will be 0
-                        output.add("D=D-M");
-                        output.add(&format!("@{}", larger_label));
-                        output.add("D;JGT"); // if D>0, jump to larger_label
+                        writer.add_str("D=D-M");
+                        writer.add_str(&format!("@{}", larger_label));
+                        writer.add_str("D;JGT"); // if D>0, jump to larger_label
 
                         // if D<=M, D will be 0 and jump to finish_label
-                        output.add("D=0");
-                        output.add(&format!("@{}", finish_label));
-                        output.add("0;JMP"); // jump to finish_label
+                        writer.add_str("D=0");
+                        writer.add_str(&format!("@{}", finish_label));
+                        writer.add_str("0;JMP"); // jump to finish_label
 
-                        output.add(&format!("({})", larger_label));
-                        output.add("D=-1");
+                        writer.add_str(&format!("({})", larger_label));
+                        writer.add_str("D=-1");
 
-                        output.add(&format!("({})", finish_label));
+                        writer.add_str(&format!("({})", finish_label));
                     }
                     "gt" => {
                         // true if M > D
-                        let smaller_label = format!("{}{}", "smaller_label", output.len());
-                        let finish_label = format!("{}{}", "finish_label", output.len());
+                        let smaller_label = writer.generate_label("smaller_label");
+                        let finish_label = writer.generate_label("finish_label");
 
                         // if , jump to smaller_label and D will be 0
-                        output.add("D=M-D");
-                        output.add(&format!("@{}", smaller_label));
-                        output.add("D;JGT"); // if D>0, jump to smaller_label
+                        writer.add_str("D=M-D");
+                        writer.add_str(&format!("@{}", smaller_label));
+                        writer.add_str("D;JGT"); // if D>0, jump to smaller_label
 
                         // if D>=M, D will be 0 and jump to finish_label
-                        output.add("D=0");
-                        output.add(&format!("@{}", finish_label));
-                        output.add("0;JMP"); // jump to finish_label
+                        writer.add_str("D=0");
+                        writer.add_str(&format!("@{}", finish_label));
+                        writer.add_str("0;JMP"); // jump to finish_label
 
-                        output.add(&format!("({})", smaller_label));
-                        output.add("D=-1");
+                        writer.add_str(&format!("({})", smaller_label));
+                        writer.add_str("D=-1");
 
-                        output.add(&format!("({})", finish_label));
+                        writer.add_str(&format!("({})", finish_label));
                     }
                     _ => unreachable!(),
                 }
-                output.remove_2_and_push_data();
+                writer.remove_2_and_push_data();
             }
             "add" | "sub" | "and" | "or" => {
-                output.get_2_numbers_from_stack();
+                writer.get_2_numbers_from_stack();
                 match split_line[0] {
-                    "add" => output.add("D=D+M"),
-                    "sub" => output.add("D=M-D"),
-                    "and" => output.add("D=M&D"),
-                    "or" => output.add("D=M|D"),
+                    "add" => writer.add_str("D=D+M"),
+                    "sub" => writer.add_str("D=M-D"),
+                    "and" => writer.add_str("D=M&D"),
+                    "or" => writer.add_str("D=M|D"),
                     _ => unreachable!(),
                 }
-                output.remove_2_and_push_data();
+                writer.remove_2_and_push_data();
             }
             "neg" | "not" => {
-                output.add("@SP");
-                output.add("A=M");
-                output.add("A=A-1");
+                writer.add_str("@SP");
+                writer.add_str("A=M");
+                writer.add_str("A=A-1");
                 match split_line[0] {
-                    "neg" => output.add("M=-M"),
-                    "not" => output.add("M=!M"),
+                    "neg" => writer.add_str("M=-M"),
+                    "not" => writer.add_str("M=!M"),
                     _ => unreachable!(),
                 }
-                output.add("D=A+1");
-                output.add("@SP");
-                output.add("M=D");
+                writer.add_str("D=A+1");
+                writer.add_str("@SP");
+                writer.add_str("M=D");
             }
             "pop" => unimplemented!(),
             _ => unimplemented!("{}", line),
         }
     }
-    Ok(output)
+    Ok(writer.output)
 }
 
-trait PushStringRef {
-    fn add(&mut self, s: &str);
+struct VirtualMachineWriter {
+    output: Vec<String>,
+}
+
+impl VirtualMachineWriter {
+    fn add_str(&mut self, s: &str) {
+        self.output.push(s.to_owned());
+    }
+
+    fn push(&mut self, segment: &str, index: &str) -> Result<(), ()> {
+        match segment {
+            "constant" => match index.parse::<i16>() {
+                Ok(n) => {
+                    self.add_str(&format!("@{}", n));
+                    self.add_str("D=A");
+                    self.add_str("@SP");
+                    self.add_str("A=M");
+                    self.add_str("M=D");
+                    self.add_str("A=A+1");
+                    self.add_str("D=A");
+                    self.add_str("@SP");
+                    self.add_str("M=D");
+                    Ok(())
+                }
+                Err(_) => Err(()),
+            },
+            _ => unimplemented!(),
+        }
+    }
+
+    fn generate_label(&self, label: &str) -> String {
+        format!("{}{}", label, self.output.len())
+    }
+
     fn get_2_numbers_from_stack(&mut self) {
-        self.add("@SP");
-        self.add("A=M");
-        self.add("A=A-1");
-        self.add("D=M");
-        self.add("A=A-1");
+        self.add_str("@SP");
+        self.add_str("A=M");
+        self.add_str("A=A-1");
+        self.add_str("D=M");
+        self.add_str("A=A-1");
     }
 
     fn remove_2_and_push_data(&mut self) {
-        self.add("@SP");
-        self.add("A=M");
-        self.add("A=A-1");
-        self.add("A=A-1");
-        self.add("M=D");
-        self.add("D=A+1");
-        self.add("@SP");
-        self.add("M=D");
-    }
-}
-
-impl PushStringRef for Vec<String> {
-    fn add(&mut self, s: &str) {
-        self.push(s.to_owned());
+        self.add_str("@SP");
+        self.add_str("A=M");
+        self.add_str("A=A-1");
+        self.add_str("A=A-1");
+        self.add_str("M=D");
+        self.add_str("D=A+1");
+        self.add_str("@SP");
+        self.add_str("M=D");
     }
 }
 
